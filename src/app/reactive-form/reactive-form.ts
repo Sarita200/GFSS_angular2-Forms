23e6d212f6f3d701af8b6d113930c36a8fb0c1b5
Sarita200 <saritasadgir1@gmail.com>
2025-07-27 14:58:43 +0530

    reactive form created duccessfully

--- src/app/reactive-form/reactive-form.ts
+++ src/app/reactive-form/reactive-form.ts
@@ -1,11 +1,33 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { group } from 'console';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule ,JsonPipe],
   templateUrl: './reactive-form.html',
   styleUrl: './reactive-form.css'
 })
 export class ReactiveForm {
 
+  studentObj :FormGroup = new FormGroup({
+    firstName :new FormControl('',[Validators.required,Validators.minLength(3)]),
+    lastName : new FormControl(''),
+    city :new FormControl('', [Validators.required , Validators.minLength(3) ]),
+    address :new FormControl(''),
+    zip :new FormControl('0'),
+    isActive :new FormControl(false)
+  })
+
+  formValue :any;
+
+  saveForm(){
+    this.formValue = this.studentObj.value;
+  }
+
+  resetForm(){
+
+  }
 }
